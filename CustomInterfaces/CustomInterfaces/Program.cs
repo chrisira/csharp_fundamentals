8021a7b199ca635598a99fafc8fd1ceb9ee2fd41
Bishop <christianiradukunda6@gmail.com>
2023-05-26 21:58:34 +0200

    Done creating instances of the classes and added destroyables to the list

--- CustomInterfaces/CustomInterfaces/Program.cs
+++ CustomInterfaces/CustomInterfaces/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            //creating 2 objects of type chair
+            Chair officeChair = new Chair("Brown", "Plastic");
+            Chair gamingChair = new Chair("Red", "Wood");
+
+            //creating new object of type car
+            Car damagedCar = new Car(80f, "Blue");
+
+            // adding 2 chairs near the damaged car
+
+            // add the two chairs to the IDestroyable list of the car
+            // so that we destroy the car and destroyable objects
+            // that near the call will get destroyed as well
+
+            damagedCar.DestroyablesNearby.Add(officeChair);
+            damagedCar.DestroyablesNearby.Add(gamingChair);
+
+
+
+
+
+
+
         }
     }
 }
