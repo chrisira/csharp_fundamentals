194aba7e33d27e5f7d3cb277fe190371568241bd
Bishop <christianiradukunda6@gmail.com>
2023-05-26 19:34:07 +0200

    Introduction to interfaces

---
+++ Intefaces/Intefaces/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intefaces
+{
+    internal class Ticket:IEquatable<Ticket>
+    {
+        public int DurationInHours { get; set; }
+
+        //.simple constructor
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+                
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+
+        }
+    }
+}
