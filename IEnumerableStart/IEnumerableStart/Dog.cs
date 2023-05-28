2ad9bacff5391bfeaa83c0fcc685941e413d0dd3
Bishop <christianiradukunda6@gmail.com>
2023-05-28 13:15:27 +0200

    Done creating the example classes that we will use as reference point

---
+++ IEnumerableStart/IEnumerableStart/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableStart
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+
+        public bool IsNaughtyDog { get; set; }
+
+
+        //adding a constructor
+        public Dog(string name, bool isNaughtyDog) {
+            this.Name= name;
+            this.IsNaughtyDog= isNaughtyDog;
+        }
+        //adding the method that will count how many numbers a dog get treated
+
+        public void GiveTreat(int NumberOfTreats)
+        {
+            // adding the message to be displayed each time the dog get treated
+            Console.WriteLine("Dog : {0} said wouf {1} times .",Name,NumberOfTreats);
+
+        }
+    }
+}
---
+++ IEnumerableStart/IEnumerableStart/DogShelter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableStart
+{
+    internal class DogShelter
+    {
+        // List of type List<Dog>
+        public List<Dog> dogs;
+
+        //adding constructor to initialize the dog with some values
+        public DogShelter()
+        {
+            // Initialize the dog list using the collection initializer
+            dogs = new List<Dog>()
+            {
+                new Dog("Casper",false),
+                new Dog("Sif",true),
+                new Dog("Oreo",false),
+                new Dog("Pixel",false)
+            };
+
+
+        }
+    }
+}
